2f08d8378fda798985d797df7f388b6115534d1e
fueher <35857172+fueher@users.noreply.github.com>
2018-02-17 22:40:50 -0600

    Updated Racer.java to follow our UML

--- Artifacts/Documents/Racer/Racer.java
+++ Artifacts/Documents/Racer/Racer.java
@@ -2,34 +2,52 @@ import java.time.LocalTime;
 
 public class Racer {
 
-	private LocalTime start; // start time
-	private LocalTime stop; // stop time
-	private int iD; // racer's identity number
+	private LocalTime startTime; // racer's start time
+	private LocalTime endTime; // racer's end time
+	private int bibNum; // racer's bib number
+	private boolean dnf; // did not finish state
 	
-	//private LocalTime duration; //duration between stop and start time
+	//private LocalTime duration; //duration between endTime and start time
 	
 	//constructor//
-	public Racer(int id){ this.iD = id; }
+	protected Racer(){}
+	protected Racer(int bn){ this.bibNum = bn; }
+	protected Racer(int bibNum, LocalTime sTime){
+		this.bibNum = bn;
+		this.startTime = sTime;
+	}
+	protected Racer(int bibNum, LocalTime sTime, LocalTime fTime){
+		this.bibNum = bn;
+		this.startTime = sTime;
+		this.endTime = fTime;
+	}
 	
 	//setters//
-	public void setStartTime(LocalTime t){ start = t; }
+	protected void setStart(LocalTime sTime){ this.startTime = sTime; }
 	
-	public void setStopTime(LocalTime t) { stop = t; }
+	protected void setFinish(LocalTime fTime) { this.endTime = fTime; }
+	
+	protected boolean setBib(int bibNum){ this.bibNum = bibNum; }
+	
+	protected void setDNF(boolean status) { this.dnf = status; }
+	
 	
 	//getters//
-	public LocalTime getStartTime(){ return start; }
+	protected LocalTime getStartTime(){ return this.startTime; }
 	
-	public LocalTime getStopTime(){ return stop; }
+	protected LocalTime getEndTime(){ return this.endTime; }
 	
-	public int getID() { return this.iD; }
+	protected boolean getDNF(){ return this.dnf; }
 	
-	public LocalTime getDuration(){
+	protected int getBibNum() { return this.bibNum; }
+	
+	protected LocalTime getTime(){
 		
-		if(start == null || stop == null) throw new NullPointerException("Cannot get duration, there is either no start/stop time for the racer: " + iD);
+		if(startTime == null || endTime == null) throw new NullPointerException("Cannot get duration, there is either no start/endTime time for the racer: " + bibNum);
 		
-		LocalTime duration = stop.minusHours(start.getHour());
-		duration = duration.minusMinutes(start.getMinute());
-		duration = duration.minusSeconds(start.getSecond());
+		LocalTime duration = endTime.minusHours(startTime.getHour());
+		duration = duration.minusMinutes(startTime.getMinute());
+		duration = duration.minusSeconds(startTime.getSecond());
 		
 		//System.out.println(duration.toString());
 		//this.duration = duration;
@@ -38,8 +56,8 @@ public class Racer {
 	}
 	
 	// Test run to save system time to the racer's time //
-	public void startTime(){ start = java.time.LocalTime.now(); } 
+	protected void startTime(){ startTime = java.time.LocalTime.now(); } 
 
-	public void stoptime(){ stop = java.time.LocalTime.now(); }
+	protected void endTime(){ endTime = java.time.LocalTime.now(); }
 	
 }
