800a76cda370983eb9cf1d18e05bb9c165eb337f
Fue Her <fher@uwm.edu>
2018-02-20 20:09:54 -0600

    Added Javadoc to Racer.java, updated Driver.java to test run Racer.java

--- src/Chronotimer/Driver.java
+++ src/Chronotimer/Driver.java
@@ -1,4 +1,4 @@
-package chronoTimer;
+package Chronotimer;
 
 import java.util.Scanner;  
 public class Driver {
@@ -7,23 +7,26 @@ public class Driver {
 	public static void main(String[] args){
 		
 		Racer r1 = new Racer(1234);
-		
+	
+		//Simple test run on the Racer class, Racer class may subject to change when Time object is implemented.
 		
 		r1.startTime();
 		System.out.println("Recorded in military time: HH:mm:ss.sss");
-		System.out.print("Racer: '" + r1.getID() + "' start time is ");
+		System.out.print("Racer: '" + r1.getBibNum() + "' start time is ");
+		r1.startTime();
 		System.out.println(r1.getStartTime()); // starts the timer
 		
 		System.out.println("Press ENTER to stop time.");
 		Scanner input = new Scanner(System.in);
 		input.nextLine();
-		r1.stoptime(); // stops the timer
+		r1.endTime();; // stops the timer
 		
-		System.out.print("Racer: '" + r1.getID() + "' end time is ");
-		System.out.println(r1.getStopTime());
+		System.out.print("Racer: '" + r1.getBibNum() + "' end time is ");
+		r1.endTime();
+		System.out.println(r1.getEndTime());
 		
-		System.out.print("\nRacer: '" + r1.getID() + "' duration time is ");
-		System.out.println(r1.getDuration());
+		System.out.print("\nRacer: '" + r1.getBibNum() + "' duration time is ");
+		System.out.println(r1.getTime());
 		
 		input.close();
 	}
--- src/Chronotimer/Racer.java
+++ src/Chronotimer/Racer.java
@@ -1,4 +1,4 @@
-package chronoTimer;
+package Chronotimer;
 
 import java.time.LocalTime;
 
@@ -15,34 +15,87 @@ public class Racer {
 	protected Racer(){}
 	protected Racer(int bn){ this.bibNum = bn; }
 	protected Racer(int bibNum, LocalTime sTime){
-		this.bibNum = bn;
+		this.bibNum = bibNum;
 		this.startTime = sTime;
 	}
 	protected Racer(int bibNum, LocalTime sTime, LocalTime fTime){
-		this.bibNum = bn;
+		this.bibNum = bibNum;
 		this.startTime = sTime;
 		this.endTime = fTime;
 	}
 	
 	//setters//
+	
+	/**
+	 * Set the start time to this racer.
+	 * @param sTime start time
+	 */
 	protected void setStart(LocalTime sTime){ this.startTime = sTime; }
 	
+	/**
+	 * Set the finish time to this racer.
+	 * @param fTime finish time
+	 */
 	protected void setFinish(LocalTime fTime) { this.endTime = fTime; }
 	
-	protected boolean setBib(int bibNum){ this.bibNum = bibNum; }
+	/**
+	 * Set a bib number to the Racer
+	 * @param bibNum bib number to assign to this racer
+	 * @param racers list of racers to check if this bib number is already assigned
+	 * @return True if this racer has been assign to the bib number and false if
+	 * the there is a racer with the bib number already. 
+	 */
+	protected boolean setBib(int bibNum, Racer[] racers){
+		if(bibNum < 0) throw new IllegalArgumentException("Cannot assign negative number to racer.");
+		if(racers != null){
+			for(Racer r: racers){
+				if(r.bibNum == bibNum)
+					return false;
+			}
+		}
+		
+		this.bibNum = bibNum;
+		return true;
+	}
 	
+	/**
+	 * Set the status of "Did Not Finish" state to this racer 
+	 * @param status Status of what the racer state is. True if this racer did not finish and 
+	 * false if this racer did finish.
+	 */
 	protected void setDNF(boolean status) { this.dnf = status; }
 	
 	
 	//getters//
+	
+	/**
+	 * Get this racer's start time
+	 * @return Racer's start time
+	 */
 	protected LocalTime getStartTime(){ return this.startTime; }
 	
+	/**
+	 * Get this racer's end time.
+	 * @return Racer's end time.
+	 */
 	protected LocalTime getEndTime(){ return this.endTime; }
 	
+	/**
+	 * Get racer's status if it is finished with the run
+	 * @return True if racer did not finish and false if racer did finish.
+	 */
 	protected boolean getDNF(){ return this.dnf; }
 	
+	/**
+	 * Get this racer's bib number
+	 * @return Racer's bib number (int)
+	 */
 	protected int getBibNum() { return this.bibNum; }
 	
+	/**
+	 * Get the difference between this racer's finish time and start time (Finish Time - Start Time)
+	 * @return Duration of the racer's start and finish time
+	 */
 	protected LocalTime getTime(){
 		
 		if(startTime == null || endTime == null) throw new NullPointerException("Cannot get duration, there is either no start/endTime time for the racer: " + bibNum);
@@ -57,7 +110,7 @@ public class Racer {
 		return duration;
 	}
 	
-	// Test run to save system time to the racer's time //
+	// Test run to save system time to the racer's time, and will subject to change when Time object is implemented //
 	protected void startTime(){ startTime = java.time.LocalTime.now(); } 
 
 	protected void endTime(){ endTime = java.time.LocalTime.now(); }
