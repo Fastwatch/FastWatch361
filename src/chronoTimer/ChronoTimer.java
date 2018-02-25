624ed4342583586344d227bfacc64859f397f47f
fueher <fueher@CCL-GMLW110-12.ad.uwm.edu>
2018-02-25 13:57:22 -0600

    Pushing the fix for parsing string issue again from yesterday.

--- src/chronoTimer/ChronoTimer.java
+++ src/chronoTimer/ChronoTimer.java
@@ -25,17 +25,17 @@ public class ChronoTimer {
 	private ChronoTimerSimulator sim; // chrono timer simulator
 	private boolean gotEvent; // their test text file has a EVENT command so I added the flag since we are only using Run
 	
-	
-	private boolean showMessage = false; //  TRUE - print invalid sysout msg, FALSE - print no invalid sysout msg 
+	// used for testing
+	private boolean showMessage = true; //  TRUE - print invalid sysout msg, FALSE - print no invalid sysout msg 
 	/**
 	 * Helper method for printing invalid input messages and always return false. 
 	 * Used in methods that requires a boolean return value of false.
 	 * @param message message to print what was invalid/incorrect
 	 * @return false always
 	 */
-	private boolean invalidInput(String message){
+	private boolean printMessage(String message){
 		if(showMessage == true){
-			System.out.println(message);
+			System.out.println(message); 
 		}
 		return false;
 	}
@@ -133,10 +133,10 @@ public class ChronoTimer {
 //	//incomplete, needs implementations in several classes
 //	//return type?
 	public boolean execute(String c){
-		String[] commands = c.split(" ");
+		String[] commands = c.split("\\s+");
 		LocalTime time = null;
 		
-		if(commands.length < 2) return invalidInput("Not enough arguments. Need atleast 2.");
+		if(commands.length < 2) return printMessage("Not enough arguments. Need atleast 2.");
 		
 		if(powerState == true){
 			time = parseTime(commands[0]);
@@ -157,23 +157,23 @@ public class ChronoTimer {
 					break;
 					
 				case("CANCEL"): // Discard current run for racer and place racer back to queue
-					if(currentRun == null) return invalidInput("Cannot cancel run, there is currently no run.");
+					if(currentRun == null) return printMessage("Cannot cancel run, there is currently no run.");
 					try {
 						currentRun.cancel();
 					}catch(IllegalStateException e) {
-						return invalidInput("No racers on course.");
+						return printMessage(e.getMessage());
 					}
 					break;
 					
 				case("EVENT"): // Set Run to this particular event <TIME> <EVENT> <TYPE>
-					if(commands.length <= 2) return invalidInput("Need a event type. Should be the 3rd arg.");
+					if(commands.length <= 2) return printMessage("Need a event type. Should be the 3rd arg.");
 					
 					else if(commands[2].equalsIgnoreCase("IND")) {
 						gotEvent = true;
-						invalidInput("Event set to IND");
+						printMessage("Event set to IND");
 					}	
 					else
-						return invalidInput("Unsupported event type.");
+						return printMessage("Unsupported event type.");
 					break;
 					
 				case("CONN"): // CONN <sensor> <NUM>
@@ -185,12 +185,12 @@ public class ChronoTimer {
 					try {
 						newRun();
 					}catch(IllegalStateException e) {
-						return invalidInput("Cannot create new run. Must end a run first.");
+						return printMessage("Cannot create new run. Must end a run first.");
 					}
 					break;
 				
 				case("ENDRUN"): // Done with a Run
-					if(endRun() == false) return invalidInput("There is no current run.");
+					if(endRun() == false) return printMessage("There is no current run.");
 					break;
 				
 				case("RESET"): // Reset system to initial state
@@ -198,62 +198,66 @@ public class ChronoTimer {
 					break;
 					
 				case("TIME"): // Set the current time. Default time is host system time.
-					if(commands.length <= 2) return invalidInput("Need a time to set. Should be the 3rd arg.");
+					if(commands.length <= 2) return printMessage("Need a time to set. Should be the 3rd arg.");
 					time = parseTime(commands[2]);
-					if(time == null) return invalidInput("Time is is null");
+					if(time == null) return printMessage("Time is is null");
 					else
 						System.out.println("Time has been set to " + time);
 					break;
 				
 				case("NUM"): // Set	NUM<NUMBER> as the next	competitor to start.
-					if(commands.length <= 2) return invalidInput("Need a bib number to set racer. Should be the 3rd arg.");
-					if(currentRun == null) return invalidInput("Cannot add racer, create a Run first.");
+					if(commands.length <= 2) return printMessage("Need a bib number to set racer. Should be the 3rd arg.");
+					if(currentRun == null) return printMessage("Cannot add racer, create a Run first.");
 					try {
 						int bibNum = Integer.parseInt(commands[2]);
 						currentRun.num(bibNum);
 						System.out.println("Racer with bib number " + bibNum + " has been added successfully.") ;
 					}catch(NumberFormatException e) {
-						return invalidInput("Error on parsing bib number to a number.");
+						return printMessage("Error on parsing bib number to a number.");
+					}catch(IllegalStateException e) {
+						return printMessage(e.getMessage());
 					}
 					break;
 				
 				case("TOG"): // Toggle the state of the channel TOG<CHANNEL>
 					try{
-						int channel = Integer.parseInt(commands[1]);
+						int channel = Integer.parseInt(commands[2]);
 						if(channel < 0 || channel >= 9){
-							return invalidInput("Channel Not Supported.");
+							return printMessage("Channel Not Supported.");
 						}
+						// do something here, unsure at this point what to toggle channel for
+						
 					}catch(NumberFormatException e){
-						return invalidInput("Error on parsing Channel to a number.");
+						return printMessage("Error on parsing Channel to a number.");
 					}
 					break;
 					
 				case("DNF"): // DNF says the run for the bib number is over, and their end time is DNF
-					if(currentRun == null) return invalidInput("Cannot set run to dnf. There is no run.");
+					if(currentRun == null) return printMessage("Cannot set run to dnf. There is no run.");
 					try {
 						currentRun.dnf(); 
 					}catch(IllegalStateException e) {
-						return invalidInput("No racers on course");
+						return printMessage(e.getMessage());
 					}
 					break;
 					
 				case("TRIG"): // Trigger channel Trig<NUM>
 					try{
-						if(commands.length <= 2) return invalidInput("Need a channel number to trigger. Should be the 3rd arg.");
+						if(commands.length <= 2) return printMessage("Need a channel number to trigger. Should be the 3rd arg.");
 						int channel = Integer.parseInt(commands[2]);
-						if(currentRun == null) return invalidInput("Cannot start/end run. Please create a run first.");
+						if(currentRun == null) return printMessage("Cannot start/end run. Please create a run first.");
 						try {
 							currentRun.trig(time, channel);
 						}catch(IllegalStateException e) {
-							return invalidInput("No Racers waiting to start.");
+							return printMessage(e.getMessage());
 						}
 					}catch(NumberFormatException e){
-						return invalidInput("Error on parsing Channel to a number.");
+						return printMessage("Error on parsing Channel to a number.");
 					}
 					break;
 					
 				case("START"): // Start trigger channel 1 (shorthand for TRIG 1)
-					if(gotEvent == false) return invalidInput("Cannot start Run. Please choose an event first.");
+					if(gotEvent == false) return printMessage("Cannot start Run. Please choose an event first.");
 					if(execute(time.toString()+" TRIG 1") == false) // recursion, that's why it needs a second return false check
 						return false;
 					break;
@@ -264,7 +268,7 @@ public class ChronoTimer {
 					break;
 					
 				case("PRINT"): // print <RUN>
-					if(currentRun == null) return invalidInput("Cannot print Run, there is no Run.");
+					if(currentRun == null) return printMessage("Cannot print Run, there is no Run.");
 					else
 						print(time, currentRun);
 					break;
@@ -296,7 +300,7 @@ public class ChronoTimer {
 		try{
 			time = LocalTime.parse(commands);
 		}catch(DateTimeParseException e){
-			invalidInput("Something went wrong when parsing time.\nNote: Time should be the first arg in format of 00:00:00");
+			printMessage("Something went wrong when parsing time.\nNote: Time should be the first arg in format of 00:00:00");
 			return null;
 		}
 		return time;
